d2dc15f382c81795f496dc24e57aa57f38dd4aa4
Uriel Carrillo <carrillouriel@outlook.com>
2019-06-26 06:45:24 -0700

    se creo una clase para hacer tableros

---
+++ Game/Assets/MyScripts/GridGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public GameObject FichaGameObject;
+    public int TotalColumnas;
+    public int TotalFilas;
+    public float PosicionXInicial;
+    public float PosicionYInicial;
+
+    void Start()
+    {
+        CrearGrid();
+    }
+
+    void CrearGrid(){
+        for(int x=0;x<TotalColumnas;x++){
+            for(int y=0;y<TotalFilas;y++){
+                //Instanciar una ficha, ponerle el nombre de su coordenada.
+                FichaGameObject.name="Ficha_"+x+","+y;
+                // "Y" es negativa para ordenar mejor las filas y columnas.
+                Instantiate(FichaGameObject, new Vector3(PosicionXInicial+x, PosicionYInicial-y, 0), Quaternion.identity);
+            }
+        }
+    }
+}
