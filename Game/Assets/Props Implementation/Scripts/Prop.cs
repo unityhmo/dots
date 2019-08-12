481c06869d83bc9df2d21be54656196a7dfc0fbd
Smireles <mireles001@gmail.com>
2019-08-11 19:00:36 -0700

    Props Implementation - Upgrading project to 2019.2 - Adding props assets and creating a Demostration scene.

---
+++ Game/Assets/Props Implementation/Scripts/Prop.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+  enum PropType
+  {
+    Clan = 0,
+    Quad = 1
+  }
+
+  [SerializeField]
+  private PropType _propType = PropType.Clan;
+  public Transform animatedContentWrapper;
+  [SerializeField]
+  private float _managerCallbackTimer = 0.85f;
+  [SerializeField]
+  private float _autoDestroyTimer = 1.01f;
+  private Animator _animator;
+  private PropsManager _manager;
+
+  private void Awake()
+  {
+    _animator = animatedContentWrapper.GetComponent<Animator>();
+    animatedContentWrapper.gameObject.SetActive(false);
+  }
+
+  public Transform StartUp(PropsManager manager)
+  {
+    _manager = manager;
+
+    animatedContentWrapper.gameObject.SetActive(true);
+    if (_animator)
+    {
+      _animator.Play("go");
+    }
+
+    Invoke("ManagerCallback", _managerCallbackTimer);
+    Invoke("AutoDestroy", _autoDestroyTimer);
+
+    return transform;
+  }
+
+  private void ManagerCallback()
+  {
+    if (_propType == PropType.Clan)
+    {
+      _manager.InstantiateCapturedQuad(transform.position);
+    }
+    else
+    {
+      _manager.QuadCaptureAnimationCompleted(transform.position);
+    }
+  }
+
+  private void AutoDestroy()
+  {
+    Destroy(gameObject);
+  }
+}
