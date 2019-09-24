fdb07f27c57cb9476162e11390725cce3fc9e9ff
tony <atony.mtz@gmail.com>
2019-09-24 03:35:32 -0700

    Safe & Sound :musical_note:

---
+++ Game/Assets/UI/Scripts/PlayMusic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayMusic : PlaySfx {
+    [SerializeField] private bool repeat;
+
+    public override void Play() {
+        soundManagerController.PlaySfx(audioClip, volume);
+    }
+
+    void OnDestroy() {
+        if (playWhen == SoundManagerController.EventType.BeforeDestroy) {
+            Play();
+        }
+    }
+}
---
+++ Game/Assets/UI/Scripts/PlaySfx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class PlaySfx : MonoBehaviour {
+    [SerializeField] protected SoundManagerController.EventType playWhen = SoundManagerController.EventType.Other;
+    [SerializeField] [Range(0, 1)] protected float volume = 1f;
+    [SerializeField] protected AudioClip audioClip;
+
+    protected SoundManagerController soundManagerController;
+
+    void Start() {
+        GameObject soundManager = GameObject.FindWithTag("SoundManager");
+
+        if (audioClip == null) {
+            throw new ArgumentException("No `AudioClip` added to script");
+        }
+
+        if (soundManager == null) {
+            throw new ArgumentException("No `SoundManager` object found");
+        }
+
+        soundManagerController = soundManager.GetComponent<SoundManagerController>();
+
+        if (soundManagerController == null) {
+            throw new ArgumentException("No `SoundManager` object found");
+        }
+
+        if (playWhen == SoundManagerController.EventType.StartScript) {
+            Play();
+        }
+    }
+
+    void OnDestroy() {
+        if (playWhen == SoundManagerController.EventType.BeforeDestroy) {
+            Play();
+        }
+    }
+
+    public virtual void Play() {
+        soundManagerController.PlayMusic(audioClip, volume);
+    }
+}
