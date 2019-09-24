e32c1a1e177707e0f0c1fa4fc271da4093174555
tony <atony.mtz@gmail.com>
2019-09-24 03:57:15 -0700

    Safe & Sound :musical_note:

---
+++ Game/Assets/UI/Scripts/PlaySfx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class PlaySfx : MonoBehaviour {
+    [SerializeField] protected AudioManagerController.EventType playWhen = AudioManagerController.EventType.Other;
+    [SerializeField] [Range(0, 1)] protected float volume = 1f;
+    [SerializeField] protected AudioClip audioClip;
+
+    protected AudioManagerController audioManagerController;
+
+    void Start() {
+        GameObject soundManager = GameObject.FindWithTag("AudioManager");
+
+        if (audioClip == null) {
+            throw new ArgumentException("No `AudioClip` added to script");
+        }
+
+        if (soundManager == null) {
+            throw new ArgumentException("No `AudioManager` object found");
+        }
+
+        audioManagerController = soundManager.GetComponent<AudioManagerController>();
+
+        if (audioManagerController == null) {
+            throw new ArgumentException("No `AudioManager` object found");
+        }
+
+        if (playWhen == AudioManagerController.EventType.StartScript) {
+            Play();
+        }
+    }
+
+    void OnDestroy() {
+        if (playWhen == AudioManagerController.EventType.BeforeDestroy) {
+            Play();
+        }
+    }
+
+    public virtual void Play() {
+        audioManagerController.PlayMusic(audioClip, volume);
+    }
+}
