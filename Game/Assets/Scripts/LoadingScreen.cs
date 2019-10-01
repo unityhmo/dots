7ce568e6de1c025a2fcd153084d1e365f86d86a5
Uriel Carrillo <carrillouriel@gmail.com>
2019-09-30 23:45:39 -0700

    se agregaron las otras imagenes de transcisiones y se puso la logica de cambiar el dibujo entre las transcisiones

--- Game/Assets/Scripts/LoadingScreen.cs
+++ Game/Assets/Scripts/LoadingScreen.cs
@@ -5,10 +5,13 @@ public class LoadingScreen : MonoBehaviour
 {
   public Slider slider;
   private SceneTransition _transitioner;
+  public GameObject holderClanSprites;
+  public Sprite[] clanSprites;
 
   public void StartUp(SceneTransition transitioner)
   {
     _transitioner = transitioner;
+    SetClanSprite();
   }
 
   private void Update()
@@ -19,5 +22,11 @@ public class LoadingScreen : MonoBehaviour
     }
 
     slider.value = _transitioner.Progress;
+  }
+
+  void SetClanSprite(){
+     int rand = UnityEngine.Random.Range(0, clanSprites.Length);
+     holderClanSprites.GetComponent<Image>().sprite = clanSprites[rand];
+     holderClanSprites.GetComponent<Image>().SetNativeSize();
   }
 }
